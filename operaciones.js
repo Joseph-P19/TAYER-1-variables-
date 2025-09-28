664e1721483c952164743ac629864dfcbf95fd05
Joseph-P19 <josdphpuerta@gmail.com>
2025-09-28 17:56:05 -0500

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,29 @@
+//*  punto 1
+/* let nota1 = parseFloat(prompt("ingrese nota 1"));
+let nota2 = parseFloat(prompt("ingrse nota 2"));
+let nota3 = parseFloat(prompt("ingrse nota 3 "));
+
+let suma = nota1 + nota2 + nota3;
+let promedio = suma / 3;
+console.log("el resultado de la nota es:" + suma);
+document.writeln("el resultado de la nota es:" + suma);
+document.writeln("<br> el resultado de la nota es:" + promedio); */
+ 
+
+//* 
+//SEGUNDO PUNTO
+
+/* let cateto1 = parseFloat(prompt("Ingrese el valor del cateto 1"))
+let cateto2 = parseFloat(prompt("Ingrese el valor del cateto 2"))
+let hipotenusa = Math.sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
+console.log("El resultado de la hipotenusa es:", hipotenusa)
+document.writeln("El resultado de la hipotenusa es:", hipotenusa)  */
+
+//*
+//TErCER PUNTO
+
+/* let Grados = parseFloat(prompt("Ingrese la cantidad de grados centigrados a convertir a Farenheit"))
+let Farenheit = (Grados * 9/5) + 32;
+console.log("El resultado de la conversion es:", Farenheit)
+document.writeln("El resultado de la conversion es:", Farenheit) 
+ */
